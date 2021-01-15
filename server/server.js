d3b777c81e3ea79b1da1cd3111a68cace6846a51
jsandleraol <46496940+jsandleraol@users.noreply.github.com>
2021-01-15 13:13:47 -0800

    server setup

--- server/server.js
+++ server/server.js
@@ -12,9 +12,9 @@ const app = express()
 //   })
 
 
-app.use(express.static("src"))
+app.use(express.static("build"))
 
-app.get('*', function (req, res) {
+app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, '../build/index.html'))
   })
 
