a15dc2e3f27db7f5a82e42fa2e68d490038fad7c
jsandleraol <46496940+jsandleraol@users.noreply.github.com>
2021-01-15 13:29:26 -0800

    server setup

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const app = express()
 app.use(express.static("build"))
 
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, '../build/index.html'))
+    res.sendFile(path.join(__dirname, 'build', 'index.html'))
   })
 
 app.listen(process.env.PORT || 5000, () => {
