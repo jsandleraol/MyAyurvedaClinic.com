352623575451ac18643bb9478927631689616ff5
jsandleraol <46496940+jsandleraol@users.noreply.github.com>
2021-01-15 10:07:00 -0800

    set server

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const PORT = process.env.PORT || 5000
+const http = require('http')
+const path = require('path')
+
+const app = express()
+
+app.use('/static', express.static(path.join(__dirname, 'src')))
+
+app.listen(process.env.PORT || 5000, () => {
+    console.log(`listening on port: ${PORT}`)
+  })
