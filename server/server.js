13f53d7f3ca555caad9e7d1840e1c548f73420a8
jsandleraol <46496940+jsandleraol@users.noreply.github.com>
2021-01-15 10:27:29 -0800

    server setup

--- server/server.js
+++ server/server.js
@@ -4,7 +4,15 @@ const path = require('path')
 
 const app = express()
 
-app.use('/static', express.static(path.join(__dirname, 'src')))
+// app.get('/*', function (req, res) {
+//     res.sendFile(path.join(__dirname, '../build/index.html'), function (err) {
+//       if (err) {
+//         console.log(err);
+//       }
+//     })
+//   })
+
+app.use('/static', express.static(path.join(__dirname, '../src')))
 
 app.listen(process.env.PORT || 5000, () => {
     console.log(`listening on port: ${PORT}`)
